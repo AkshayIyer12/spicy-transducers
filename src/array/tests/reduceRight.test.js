e490440df2246e0f13ac24e8ce8db945baa878d2
AkshayIyer12 <c.m.akshay.iyer@gmail.com>
2018-10-19 02:26:38 +0530

    Add reduce right function in array

---
+++ src/array/tests/reduceRight.test.js
@@ -0,0 +1,12 @@
+import reduceRight from '../reduceRight';
+
+describe('array/reduceRight', () => {
+  it('uses the built in array reduceRight method', () => {
+      const input = [10, 12, 14, 15];
+    const defaultValue = 100;
+    const expected = 152;
+    const reduceFn = (acc, _) => acc + _;
+
+    expect(reduce(reduceFn, defaultValue)(input)).toEqual(expected);
+  });
+});
