6acb950bf4e9bb4f503afe35d498251fb08d8458
Jovan Erčić <jokka@users.noreply.github.com>
2018-10-27 00:14:49 +0530

    Remove extra space
    Co-Authored-By: AkshayIyer12 <c.m.akshay.iyer@gmail.com>

--- src/array/tests/last.test.js
+++ src/array/tests/last.test.js
@@ -3,6 +3,6 @@ import last from '../last';
 describe('array/last', () => {
   it('should get value from array at n-1th index', () => {
     const arr = [100, 200, 300];
-      expect(last(arr)).toEqual(300);
+    expect(last(arr)).toEqual(300);
   })
 })
